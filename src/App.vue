c1f734254ab244c9a1caad1638870f7d3f529c21
battistaennio <ennio.battista99@gmail.com>
2024-07-05 16:10:09 +0200

    feat: add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 
+  export default{
+    name: "App",
+    components: {
+      AppHeader,
+
+    }
+  }
 </script>
 
 <template>
-
+  <AppHeader />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,93 @@
+90<script>
+    export default{
+        name: "AppHeader",
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="/src/assets/dc-logo.png" alt="DC logo">
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="nav">
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+
+header{
+    height: 90px;
+
+    nav{
+        @include center("between");
+
+        ul{
+            display: flex;
+
+            img{
+                width: 60px;
+                margin: 15px 0;
+            }
+
+        }
+
+        .nav{
+
+            a{
+                text-transform: uppercase;
+                font-size: 0.6rem;
+                margin: 0 15px;
+                padding-bottom: 30px;
+                font-weight: 600;
+                &:hover{
+                    border-bottom: 5px solid $primary-color;
+                }
+
+            }
+        }
+    }
+}
+</style>
